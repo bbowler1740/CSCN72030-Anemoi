1c742d7947dfffd9a0195a05215e945e6f3e1b36
Brayden <bbowler1740@conestogac.on.ca>
2021-10-22 11:02:18 -0400

    Small update to the Sensors class including: Class properties Parameterized constructor Print/Save/Load functionality

--- CSCN72030-Anemoi/Sensors.cs
+++ CSCN72030-Anemoi/Sensors.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace CSCN72030_Anemoi
 {
-    class Sensors
+    class Sensors //Specific sensor types: temperature, precipitation, air pressure, sunlight, ground, humidity, wind direction, wind speed
     {
+        public int SensorID { get; private set; } = -1;
+        public string SensorNickName { get; set; } = "Unset";
+        public string SensorLocation { get; set; } = "Unset";
+        public Sensors(int sensorID, string sensorNickName, string sensorLocation) //Parameterized
+        {
+            this.SensorID = sensorID;
+            this.SensorNickName = sensorNickName;
+            this.SensorLocation = sensorLocation;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Sensor ID: {0}", this.SensorID);
+            Console.WriteLine("Sensor Nickname: {0}", this.SensorNickName);
+            Console.WriteLine("Sensor Location: {0}", this.SensorLocation);
+            return;
+        }
+        public void Save(string fileName)
+        {
+
+            //How do we want to save? Binary, XML, JSON?
+
+        }
+        public Sensors Load()
+        {
+
+            //How do we want to load? Binary, XML, JSON?
+
+        }
+
+
     }
 }
