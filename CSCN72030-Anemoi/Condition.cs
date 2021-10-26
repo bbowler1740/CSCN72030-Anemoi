45ef5af2f7ff2f23662a5f503a2529d95e9e4f5e
Kevin Mcdaniel <Kmcdaniel3523@conestogac.on.ca>
2021-10-26 15:01:47 -0400

    Added access to manage ConditionalActions

--- CSCN72030-Anemoi/Condition.cs
+++ CSCN72030-Anemoi/Condition.cs
@@ -8,8 +8,16 @@ namespace CSCN72030_Anemoi
 {
     class Condition
     {
-        //field: Sensor
-        //properties: high threshold, low threshold, isBetween
+        private Sensor sensor;
+        private bool isBetween;
+        private int lowThreshold;
+        private int highThreshold;
+
+        public Sensor Sensor { get => sensor; set => sensor = value; }
+        public bool IsBetween { get => isBetween; set => isBetween = value; }
+        public int LowThreshold { get => lowThreshold; set => lowThreshold = value; }
+        public int HighThreshold { get => highThreshold; set => highThreshold = value; }
+
         //functions: Check(), constructor(Sensor, isBetween, high, low)
     }
 }
--- CSCN72030-Anemoi/ConditionAnalyzer.cs
+++ CSCN72030-Anemoi/ConditionAnalyzer.cs
@@ -9,12 +9,101 @@ namespace CSCN72030_Anemoi
     class ConditionAnalyzer
     {
         private List<ConditionalAction> conditionalActions = new List<ConditionalAction>();
-        //has a device list of Sensors and Devices
+        private DeviceList deviceList;
+
+        public DeviceList DeviceList { get => deviceList; set => deviceList = value; }
+
+        public bool AddConditionalAction(string name, List<Condition> conditions, List<Action> actions, bool isEnabled = true)
+        {
+            if (conditionalActions.Exists(x => x.Name == name))
+            {
+                return false;
+            }
+            conditionalActions.Add(new ConditionalAction(name, conditions, actions, isEnabled));
+            return true;
+        }
+
+        public bool UpdateConditionalAction(string oldName, string newName, List<Condition> conditions, List<Action> actions, bool isEnabled)
+        {
+            if (!conditionalActions.Exists(x => x.Name == oldName) || conditionalActions.Exists(x => x.Name == newName))
+            {
+                return false;
+            }
+            var conditionalAction = conditionalActions.Where(c => c.Name == oldName).FirstOrDefault();
+            var index = conditionalActions.IndexOf(conditionalAction);
+            conditionalActions[index] = new ConditionalAction(newName, conditions, actions, isEnabled);
+            return true;
+        }
+
+        public bool RemoveConditionalAction(string name)
+        {
+            if (conditionalActions.Exists(x => x.Name == name))
+            {
+                return false;
+            }
+            var conditionalAction = conditionalActions.Where(c => c.Name == name).FirstOrDefault();
+            var index = conditionalActions.IndexOf(conditionalAction);
+            conditionalActions.RemoveAt(index);
+            return true;
+        }
+
+        public List<string> GetListOfName()
+        {
+            var listOfNames = new List<string>();
+            foreach (var conditionalAction in conditionalActions)
+            {
+                listOfNames.Add(conditionalAction.Name);
+            }
+            return listOfNames;
+        }
+
+        public List<Condition> GetConditions(string name)
+        {
+            var conditionalAction = conditionalActions.Where(c => c.Name == name).FirstOrDefault();
+            return conditionalAction.Conditions;
+        }
+
+        public List<Action> GetActions(string name)
+        {
+            var conditionalAction = conditionalActions.Where(c => c.Name == name).FirstOrDefault();
+            return conditionalAction.Actions;
+        }
+
+        public bool IsActionEnabled(string name)
+        {
+            var conditionalAction = conditionalActions.Where(c => c.Name == name).FirstOrDefault();
+            return conditionalAction.IsEnabled;
+        }
+
         //check all Enabled conditionalActions Conditions for true
-            //if true, perform Actions
+        //if true, perform Actions
 
-        //CRUD conditional actions
 
+        public string Stringify()
+        {
+            var stringifiedConditionalAnalyzer = "";
+            foreach (var conditionalAction in conditionalActions)
+            {
+                stringifiedConditionalAnalyzer += conditionalAction.Name + "{C:";
+                foreach (var condition in conditionalAction.Conditions)
+                {
+                    stringifiedConditionalAnalyzer += string.Format("({0},{1},{2},{3}),", 
+                        condition.Sensor.GetType().Name, condition.IsBetween, condition.LowThreshold, condition.HighThreshold);
+                }
+                var lastIndex = stringifiedConditionalAnalyzer.LastIndexOf(',');
+                if (lastIndex != -1)
+                {
+                    stringifiedConditionalAnalyzer = stringifiedConditionalAnalyzer.Remove(lastIndex);
+                }
+                stringifiedConditionalAnalyzer += "A:";
+                //INCOMPLETE
+            }
+            return stringifiedConditionalAnalyzer;
+        }
+        //Stringify: need all sensors/devices available, everything in conditionalActions
+        /*
+         * name{C:(Sensor,isBetween,high,low),(Sensor,isBetween,high,low)A:(Device, output),(Device, output)E:true}
+         */
 
         private class ConditionalAction
         {
@@ -24,9 +113,17 @@ namespace CSCN72030_Anemoi
             private bool isEnabled;
 
             public string Name { get => name; set => name = value; }
-            public List<Condition> Conditions { get => conditions; private set => conditions = value; }
-            public List<Action> Actions { get => actions; private set => actions = value; }
+            public List<Condition> Conditions { get => conditions; set => conditions = value; }
+            public List<Action> Actions { get => actions; set => actions = value; }
             public bool IsEnabled { get => isEnabled; set => isEnabled = value; }
+
+            public ConditionalAction(string name, List<Condition> conditions, List<Action> actions, bool isEnabled = true)
+            {
+                Name = name;
+                Conditions = conditions;
+                Actions = actions;
+                IsEnabled = IsEnabled;
+            }
         }
     }
 }
