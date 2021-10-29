fc7989f54446afb3c727d8ad2121a42f7eb49d40
Brayden <bbowler1740@conestogac.on.ca>
2021-10-29 11:55:53 -0400

    Basic changes to the Sensors class based on discussion with Kevin on 29/10/2021

--- CSCN72030-Anemoi/Sensors.cs
+++ CSCN72030-Anemoi/Sensors.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace CSCN72030_Anemoi
 {
-    class Sensors //Specific sensor types: temperature, precipitation, air pressure, sunlight, ground, humidity, wind direction, wind speed
+    class Sensors //Specific sensor types: temperature, precipitation, air pressure, sunlight, ground moisture, humidity, wind direction, wind speed, custom for user input.
     {
         public int SensorID { get; private set; } = -1;
-        public string SensorNickName { get; set; } = "Unset";
+        public string SensorNickName { get; set; } = "Unset"; //unique 
         public string SensorLocation { get; set; } = "Unset";
+        public float SensorData { get; set; } = -1; //Continuosly overridden when reading from file. Real-time updating is ideal, but perhaps consider 5 second intervals.
+
         public Sensors(int sensorID, string sensorNickName, string sensorLocation) //Parameterized
         {
             this.SensorID = sensorID;
@@ -24,19 +26,31 @@ namespace CSCN72030_Anemoi
             Console.WriteLine("Sensor Location: {0}", this.SensorLocation);
             return;
         }
+        public void ReadScenarioDataFromFile(string fileName)
+        {
+
+
+
+        }
+
+        /*
         public void Save(string fileName)
         {
 
             //How do we want to save? Binary, XML, JSON?
+            //Text
 
         }
-        public Sensors Load()
+        */
+
+        /*public Sensors Load()
         {
 
             //How do we want to load? Binary, XML, JSON?
-
+            //Text
+            
         }
-
-
+        */
+      
     }
 }
