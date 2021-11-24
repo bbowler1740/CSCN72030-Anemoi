cda95ffc0c000615463b06369e108a3f509091f5
Kevin Mcdaniel <Kmcdaniel3523@conestogac.on.ca>
2021-11-23 19:48:04 -0500

    Added the Conditional Action section of the MainPage

--- CSCN72030-Anemoi/MainPage.xaml.cs
+++ CSCN72030-Anemoi/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
+using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -36,17 +37,39 @@ namespace CSCN72030_Anemoi
             backgroundFade.VerticalAlignment = VerticalAlignment.Stretch;
             backgroundFade.HorizontalAlignment = HorizontalAlignment.Stretch;
 
+            listViewConditionalActions.ItemsSource = new List<ConditionalActionData>() { 
+                new ConditionalActionData() { Conditions = "Temperature > 10, Wind Speed < 60", Actions = "Canopy = On", Status = "Enabled" },
+                new ConditionalActionData() { Conditions = "Sunlight < 10", Actions = "Lights = On, Pool Heater = Off", Status = "Disabled" }
+            };
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-
             Location loc = e.Parameter as Location;
 
-            Stuff.Text = loc.locationName;
+            //Stuff.Text = loc.locationName;
 
             base.OnNavigatedTo(e);      //Calls the parents implementation of the fuction
+
+        }
+
+        private void btnGoLocation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoDevice(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoSensor(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoConditional(object sender, RoutedEventArgs e)
+        {
 
         }
 
@@ -55,6 +78,27 @@ namespace CSCN72030_Anemoi
             main.Children.Remove(backgroundFade);
 
             main.Children.Remove(sender);
+        }
+    }
+
+    public class ConditionalActionData
+    {
+        public string Conditions { get; set; }
+        public string Actions { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class StatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = (string)value;
+            return val == "Enabled" ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
         }
     }
 }
